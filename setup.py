5d5132e47b291be77302548bf20d5db65ba8cf86
Jani Kajala <kajala@gmail.com>
2018-04-21 13:13:02 -0500

    Release 1.0.18

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements('requirements.txt', session=False)
 
 setup(
     name='django-jbank',
-    version='1.0.17',
+    version='1.0.18',
     author=u'Jani Kajala',
     author_email='kajala@gmail.com',
     packages=find_packages(exclude=['project', 'venv']),
