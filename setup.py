cb3a2fab18bb67d38bc8acb0b6f9ea7513fa5181
Jani Kajala <kajala@gmail.com>
2019-12-03 03:43:57 -0600

    Release 3.0.27

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements('requirements.txt', session=False)
 
 setup(
     name='django-jbank',
-    version='3.0.26',
+    version='3.0.27',
     author=u'Jani Kajala',
     author_email='kajala@gmail.com',
     packages=find_packages(exclude=['project', 'venv']),
