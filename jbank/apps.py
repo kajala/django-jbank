40dbf3ad57d27daf52f050a20840881795b970c2
Jani Kajala <kajala@gmail.com>
2021-04-07 11:36:05 -0500

    django 3.2 compatibility change

--- jbank/apps.py
+++ jbank/apps.py
@@ -5,3 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class JbankConfig(AppConfig):
     name = "jbank"
     verbose_name = _("Bank Integration")
+    default_auto_field = "django.db.models.AutoField"
