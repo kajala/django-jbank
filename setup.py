8c45ab7d00afe5a53f17d99822e3f443e8a3e239
Jani Kajala <kajala@gmail.com>
2021-05-24 16:06:29 -0500

    Release 3.8.15

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements("requirements.txt", session=False)
 
 setup(
     name="django-jbank",
-    version="3.8.14",
+    version="3.8.15",
     author=u"Jani Kajala",
     author_email="kajala@gmail.com",
     packages=find_packages(exclude=["project", "venv"]),
