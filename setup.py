a172b4a3ea9faf97eae74ac6462d3b72ebdb5289
Jani Kajala <kajala@gmail.com>
2023-04-05 18:03:59 +0700

    verbug

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements("requirements.txt", session=False)
 
 setup(
     name="django-jbank",
-    version="4.0.19",
+    version="4.1.0",
     author="Jani Kajala",
     author_email="kajala@gmail.com",
     packages=find_packages(exclude=["project", "venv"]),
