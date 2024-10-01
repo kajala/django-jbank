db6fe883ac7bea498f312cd341635955fe46d1ad
Jani Kajala <kajala@gmail.com>
2024-10-01 14:56:50 -0500

    cryptography upgrade

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements("requirements.txt", session=False)
 
 setup(
     name="django-jbank",
-    version="4.4.15",
+    version="4.5.0",
     author="Jani Kajala",
     author_email="kajala@gmail.com",
     packages=find_packages(exclude=["project", "venv"]),
